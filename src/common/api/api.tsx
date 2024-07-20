3d6625a7c90872d9051b81d542f4fd539f1cc220
Alisher <alishersodiqov09@gmail.com>
2024-07-20 10:14:54 +0500

    login boshlandi

--- src/common/api/api.tsx
+++ src/common/api/api.tsx
@@ -31,13 +31,19 @@ export const question_add: string = `${base_url}question`; //admin question qush
 export const question_search: string = `${base_url}question/searchText/`; //admin or client question search qiladi
 export const question_category_all: string = `${base_url}question/list/`; //admin category buyicha question get qiladi
 
-//================CONTACT CONTROLLER====================== 5 ta api
-
+//================CONTACT CONTROLLER======================
+export const contact_all: string = `${base_url}contact`; //all contact ni get qilish shu yulni uzidan edit xam qilsa buladi va add xam
+export const contact_one: string = `${base_url}contact/`; //bitta contact ni get qilish id keladi va delete qilsa xam buladi
 
 //================CATEGORY CONTROLLER====================== 8 ta api
 
 
-//================AUTH CONTROLLER====================== 5 ta api
+//================AUTH CONTROLLER======================
+export const auth_reset_password: string = `${base_url}auth/reset-password`; //parolni qayta tiklash
+export const auth_forgot_password: string = `${base_url}auth/forgot-password`; //parolni unitish
+export const auth_activate: string = `${base_url}auth/activate`; //?????? bilmadim
+export const auth_register: string = `${base_url}auth/register`; //user register
+export const auth_login: string = `${base_url}auth/login`; //login
 
 
 //================VIDEO UPLOAD CONTROLLER====================== 4 ta api
