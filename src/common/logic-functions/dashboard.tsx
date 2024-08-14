9287efff3f909d03b9bae17b828784f105ebef27
Alisher <alishersodiqov09@gmail.com>
2024-08-14 11:24:59 +0500

    download full

--- src/common/logic-functions/dashboard.tsx
+++ src/common/logic-functions/dashboard.tsx
@@ -1,45 +1,64 @@
 import axios from 'axios';
-import { get_certificate, get_certificate_id, statistics_card, statistics_card_all, statistics_client } from '../api/api.tsx';
+import {
+  get_certificate,
+  get_certificate_id,
+  statistics_card,
+  statistics_card_all,
+  statistics_client
+} from '../api/api.tsx';
 import { config } from '../api/token.tsx';
-import { ClientDashboardStatisticsList, DashboardListStatistic, DashboardListStatisticCards } from '../../types/dashboard.ts';
+import {
+  ClientDashboardStatisticsList,
+  DashboardListStatistic,
+  DashboardListStatisticCards
+} from '../../types/dashboard.ts';
 import { consoleClear } from '../console-clear/console-clear.tsx';
 
 export const getClientDashboardStatistic = async (page: number, size: number, setClientData: (val: null | ClientDashboardStatisticsList[]) => void, setTotalPage: (val: number) => void, setIsLoading: (val: boolean) => void) => {
-  setIsLoading(true)
+  setIsLoading(true);
   try {
     const { data } = await axios.get(`${statistics_client}?page=${page}&size=${size}`, config);
     if (data.success) {
       setClientData(data.body.body);
-      setTotalPage(data.body.totalElements)
-      setIsLoading(false)
+      setTotalPage(data.body.totalElements);
+      setIsLoading(false);
     } else {
       setClientData(null);
-      setIsLoading(false)
+      setIsLoading(false);
       consoleClear();
     }
   } catch {
     consoleClear();
-    setIsLoading(false)
+    setIsLoading(false);
     setClientData(null);
   }
 };
 
+export const downloadFile = (url: string) => {
+  axios.get(url, { ...config, responseType: 'blob' })
+    .then((res) => {
+      const blob = new Blob([res.data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `Certificate.pdf`;
+      document.body.appendChild(a);
+      a.click();
+    })
+    .catch(() => consoleClear());
+};
+
 export const getClientCertificate = async (id: number, setIsLoading: (val: boolean) => void, setCertifcate: (val: any) => void) => {
-  setIsLoading(true)
+  setIsLoading(true);
   try {
     const { data } = await axios.get(`${get_certificate_id}/${id}`, config);
-    if (data.success) {
-      const { data: certificateData } = await axios.get(`${get_certificate}/${data.body}`, config);
-      if (certificateData.success) {
-        setCertifcate(certificateData.body)
-        setIsLoading(false)
-      } else setIsLoading(false)
-    } else {
-      setIsLoading(false)
+    if (data.success) downloadFile(`${get_certificate}/${data.body}`);
+    else {
+      setIsLoading(false);
       consoleClear();
     }
   } catch {
-    setIsLoading(false)
+    setIsLoading(false);
     consoleClear();
   }
 };
