4ef8556a4782833cb8d0f58b01280ef29d30c970
Alisher <alishersodiqov09@gmail.com>
2024-09-09 19:18:29 +0500

    site full completed

--- src/pages/Authentication/PublicOffer.tsx
+++ src/pages/Authentication/PublicOffer.tsx
@@ -1,9 +1,31 @@
 import { Popover } from 'antd';
 import { MdKeyboardBackspace } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import mammoth from 'mammoth';
+import { consoleClear } from '../../common/console-clear/console-clear.tsx';
 
 const PublicOffer = () => {
   const navigate = useNavigate();
+  const [docxContent, setDocxContent] = useState<string | null>(null);
+
+  useEffect(() => {
+    offerView();
+  }, []);
+
+  const offerView = async () => {
+    const fileUrl = '/oferta.docx';
+
+    try {
+      const response = await fetch(fileUrl);
+      const arrayBuffer = await response.arrayBuffer();
+      const result = await mammoth.convertToHtml({ arrayBuffer });
+      setDocxContent(result.value);
+    } catch (error) {
+      console.error('Failed to load the document.');
+      consoleClear();
+    }
+  };
 
   return (
     <>
@@ -17,22 +39,13 @@ const PublicOffer = () => {
       <h1 className="text-3xl font-bold text-center text-gray-800 dark:text-gray-100 mb-6">
         Оммавий оферта шартлари
       </h1>
-      <p className="text-gray-700 dark:text-gray-300 leading-relaxed mb-4">
-        Ушбу ҳужжат (бундан кейин "Шартнома" деб аталади) сиз ва Бизнинг Компаниямиз ўртасида тузилган оммавий оферта
-        ҳисобланади.
-      </p>
-      <p className="text-gray-700 dark:text-gray-300 leading-relaxed mb-4">
-        Шартнома шартлари қуйидагича:
-      </p>
-      <ul className="list-disc pl-5 text-gray-700 dark:text-gray-300 mb-4">
-        <li>1. Шартнома предмети</li>
-        <li>2. Томонларнинг ҳуқуқ ва мажбуриятлари</li>
-        <li>3. Шартнома шартлари ва тўловлар</li>
-        <li>4. Низоларни ҳал этиш тартиби</li>
-      </ul>
-      <p className="text-gray-700 dark:text-gray-300 leading-relaxed mb-4">
-        Мазкур Шартнома сиз томонидан қабул қилиниши билан кучга киради.
-      </p>
+
+      {docxContent && (
+        <div
+          className={`w-full max-h-[600px] border border-slate-200 dark:border-slate-500 rounded-md p-8 overflow-y-auto bg-white dark:bg-black`}
+          dangerouslySetInnerHTML={{ __html: docxContent }}
+        />
+      )}
     </>
   );
 };
