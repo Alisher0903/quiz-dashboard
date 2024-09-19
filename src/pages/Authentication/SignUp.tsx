e645be640cee2477fb8b2f506bd023be10133a36
omadbekolimov <omadbekolimov96@gmail.com>
2024-09-19 11:36:28 +0500

    bo'ldi number qilindi

--- src/pages/Authentication/SignUp.tsx
+++ src/pages/Authentication/SignUp.tsx
@@ -288,8 +288,14 @@ const SignUp = () => {
                   <div className="relative">
                     <input
                       required
+                      type="text"
                       value={phoneNumber}
-                      onChange={e => setPhoneNumber(e.target.value)}
+                      onChange={e => {
+                        const value = e.target.value;
+                        if (!isNaN(value) && value.length <= 15) { 
+                          setPhoneNumber(value);
+                        }
+                      }}
                       placeholder="Телефон рақамни киритинг"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                     />
